9d3e62c8ef09d4247000f7af104dcdb34bbd563a
TAMZID <tamzid2233@gmail.com>
2025-09-25 14:26:56 +0600

    added authonication apis

--- controllers/userControllers/userControllers.js
+++ controllers/userControllers/userControllers.js
@@ -1,12 +1,42 @@
 const { client } = require("./../../config/mongoDB");
-const db = client.db('techLight');
-const usersCollections = db.createCollection('users');
+const db = client.db("techLight");
+const usersCollections = db.collection("users");
 
-const postUsers = (req,res) => {
-    const data = req.body;
-    console.log(data);
-    res.send({message: "This is working"})
-}
+//Register User
+const registerUser = async (req, res) => {
+  try {
+    const userData = req.body;
 
+    // Checking user already exist or not
+    const isExist = await usersCollections.findOne({ email: userData?.email });
+    if (isExist) {
+      return res.status(400).send({ message: "User already exists" });
+    }
 
-module.exports = { postUsers, }
+    //Create new User
+    userData.role = "user";
+    userData.created_at = new Date();
+    const result = await usersCollections.insertOne(userData);
+    res.status(201).send(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "Internal server error" });
+  }
+};
+
+//Login User
+const loginUser = async (req, res) => {
+  try {
+    const email = req.params.email;
+    const decodedEmail = req.decoded;
+    if (email !== decodedEmail) {
+      return res.status(401).send({ message: "Unauthorize access" });
+    }
+    const user = await usersCollections.findOne({ email });
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send({ message: "Internal server error" });
+  }
+};
+
+module.exports = { registerUser, loginUser };
---
+++ middlewares/auth.js
@@ -0,0 +1,16 @@
+
+const verifyToken = async(req, res, next) => {
+    const token = req?.headers?.authorization?.split(" ")[1];
+      if (!token) return res.status(401).send({ message: "Unauthorized Access" });
+
+      try {
+        const decoded = await admin.auth().verifyIdToken(token);
+        req.decoded = decoded?.email;
+        next();
+      } catch (err) {
+        return res.status(401).send({ message: "Unauthorized Access" });
+      }
+}
+
+
+module.exports = verifyToken;
